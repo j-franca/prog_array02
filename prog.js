5ff98362057af86974afcdf25d482f940f3aa8b4
Júlia <jfranca.contato@gmail.com>
2023-03-15 09:45:50 -0300

    PRONTO

--- prog.js
+++ prog.js
@@ -4,10 +4,11 @@ let res = document.getElementById('res')
 function array() {
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
-    res.innerHTML += cidade + ", "
+    res.innerHTML = valor_cidade
 }
 
 function array2() {
     valor_cidade.pop(cidade)
     res.innerHTML = valor_cidade
+
 }
