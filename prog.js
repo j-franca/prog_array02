9cfd23ad7627c21c7bab0bbecf783c8d81816b0d
Júlia <jfranca.contato@gmail.com>
2023-03-15 08:59:38 -0300

    funções criadas

--- prog.js
+++ prog.js
@@ -1,9 +1,10 @@
-let cidade = []
+let valor_cidade = []
 
 function array() {
-    
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.push(cidade)
 }
 
 function array2() {
-    
+    valor_cidade.pop(cidade)
 }
